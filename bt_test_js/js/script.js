f6548e4fbc9ce9045c16875bfe48567f462cbb75
quyluuxda <48611997+quyluuxda@users.noreply.github.com>
2019-05-08 10:53:45 +0700

    test js sua

--- bt_test_js/js/script.js
+++ bt_test_js/js/script.js
@@ -1,4 +1,4 @@
-//
+//Bài 1: Cho 1 mảng có độ dài lớn hơn 2, chứa các số khác nhau. Hãy viết hàm tìm ra số lớn thứ nhì trong mảng.
 document.write('Bài 1' + '<br>')
 function sapXepSo(array) {
   let kq = array.sort(function(a, b) {
@@ -7,53 +7,42 @@ function sapXepSo(array) {
   return kq[1];
 }
 document.write(sapXepSo([5, 2, 6, 0]));
-//
+//Bài 2: Cho 1 mảng các chuỗi bất kỳ. Hãy viết hàm tìm ra chuỗi có độ dài lớn nhất trong mảng.
 document.write('<br>' + 'Bài 2' + '<br>')
-function timMangDaiNhat(arr) {
+function timChuoiDaiNhat(arr) {
   for (let i = 0; i < arr.length; i++) {
-    let kq = arr.sort(function(a, b) {
-      return b - a;
-    });
-    return kq[0];
+    let max = arr[0].length
+    if (arr[i].length > max)
+    {return arr[i]};
   }
 }
-document.write(timMangDaiNhat(["sdfafkgkas", "hsfdjjsd", "fdh"]));
-//
+document.write(timChuoiDaiNhat(["we", "welcome", "welc", 'welcom']));
+//Bài 3: Viết hàm có 2 tham số là 2 chuỗi str và target, dùng để kiểm tra chuỗi str có kết thúc bởi chuỗi target hay không. Kết quả trả về là true hoặc false.
 document.write("<br>" + "Bài 3:" + "<br>");
 function bai3(str, target) {
-  if (str.substr(-1) == target.substr(-1)) {
-    document.write("true");
+  if (str.slice(-target.length) === target) {
+    return true;
   } else {
-    document.write("false");
+    return false;
   }
 }
-bai3("abcde", "de");
-//
+document.write(bai3("abcde", "de"));
+//Bài 4: Cho 1 mảng bất kỳ. Hãy kiểm tra xem trong mảng đó có phần tử nào bị lặp lại hay không.Kết quả trả về true hoặc false.
 document.write("<br>" + "Bài 4:" + "<br>");
 function bai4(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
       if (j != i){
-        if(arr[i] == arr[j]) {
-        return("True");
-      } 
+        if(arr[i] === arr[j]) {
+        return true;
+      }
     }
   }
+}return false
 }
-}
-document.write(bai4([
-  "false1",
-  "false2",
-  "false1",
-  "true4",
-  "false5",
-  "true6",
-  "false7",
-  "true8"
-]));
-
-//
-
+document.write(bai4(['abc', 3, 2, 'a', 3, 1]));
+//Bài 4 Thiếu trường hợp ([NaN, NaN])
+//Bài 5: Viết hàm truyền vào 1 chuỗi là tên thẻ HTML, kiểm tra xem trong file HTML có bao nhiêu thẻ đó. Kết quả trả về là số lượng thẻ (không bao gồm thẻ đóng), nếu không có trả về 0.
 document.write("<br>" + "Bài 5" + "<br>");
   function countTagName(tagName) {
   return(document.getElementsByTagName(tagName).length);
