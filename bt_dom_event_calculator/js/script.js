b91980ed9bfd1c754b9495293a572b86c1cf4116
quyluuxda <48611997+quyluuxda@users.noreply.github.com>
2019-05-11 14:32:17 +0700

    calcu

--- bt_dom_event_calculator/js/script.js
+++ bt_dom_event_calculator/js/script.js
@@ -1,72 +1,91 @@
-var x = '';    
+let x = '';    
 function a(id){
  return document.getElementById(id);
 }
+
 function set_num(number){
   x += number;
   a('answer').value = x;
 }
+
 function cancel(){
   x = '';
   a('answer').value = x;
 }
+
 function del(){
   x = x.slice(0, x.length-1);
   a('answer').value = x;
 }
+
 function parentheses1(){
   x += '(';
   a('answer').value = x;
 }
+
 function parentheses2(){
   x += ')';
   a('answer').value = x;
 }
+
 function addition(){
-  if(a('answer').value == '' || a('answer').value == '-' || a('answer').value == 'x' || a('answer').value == '/' ){
-        a('answer').value = '';
-        x = a('answer').value;
+  if(a('answer').value == ''){
+    a('answer').value = '';
+    x = a('answer').value;
+  }
+  if(a('answer').value.endsWith('-') == true || a('answer').value.endsWith('+') == true || a('answer').value.endsWith('x') == true){
+    x = a('answer').value;
   }else{
-      a('answer').value += "+";
-      x = a('answer').value;
-      }  
+    a('answer').value += "+";
+    x = a('answer').value;
+  }  
 }
+
 function subtraction(){
-  if(a('answer').value == '' || a('answer').value == '+' || a('answer').value == 'x' || a('answer').value == '/' ){
-        a('answer').value = '';
-        x = a('answer').value;
+  if(a('answer').value.endsWith('--') == true || a('answer').value.endsWith('+-') == true || a('answer').value.startsWith('-') == true || a('answer').value.endsWith('x-') == true || a('answer').value.endsWith('÷-') == true){
+    x = a('answer').value;
   }else{
-      a('answer').value += "-";
-      x = a('answer').value;
-      }  
+    a('answer').value += "-";
+    x = a('answer').value; 
+  }
 }
+
 function multiplication(){
-  if(a('answer').value == '' || a('answer').value == '-' || a('answer').value == '+' || a('answer').value == '/' ){
-        a('answer').value = '';
-        x = a('answer').value;
+  if(a('answer').value == ''){
+    a('answer').value = '';
+    x = a('answer').value;
+  }else
+  if(a('answer').value.endsWith('÷') == true || a('answer').value.endsWith('+') == true || a('answer').value.endsWith('x') == true || a('answer').value.endsWith('-') == true){
+    x = a('answer').value;
   }else{
-      a('answer').value += "*";
-      x = a('answer').value;
-      }  
+    a('answer').value += "x";
+    x = a('answer').value;
+  }  
 }
+
 function division(){
-  if(a('answer').value == '' || a('answer').value == '-' || a('answer').value == 'x' || a('answer').value == '+' ){
-        a('answer').value = '';
+  if(a('answer').value == ''){
+    a('answer').value = '';
+    x = a('answer').value;
+  }else
+  if(a('answer').value.endsWith('÷') == true || a('answer').value.endsWith('+') == true || a('answer').value.endsWith('x') == true || a('answer').value.endsWith('-') == true){
         x = a('answer').value;
   }else{
-      a('answer').value += "/";
+      a('answer').value += "÷";
       x = a('answer').value;
-      }  
+  }  
 }
+
+
 function dot(){
-  if(a('answer').value == '' || a('answer').value == 'x' || a('answer').value == '/' ){
-        a('answer').value = '';
-        x = a('answer').value;
+  if(a('answer').value.endsWith('.') == true){
+    x = a('answer').value
   }else{
-      a('answer').value += ".";
-      x = a('answer').value;
-      }  
+    a('answer').value += ".";
+    x = a('answer').value;
+  }
 }
+
 function sqrt(){
   if(a('answer').value == ''){
     a('answer').value = '';
@@ -76,6 +95,7 @@ function sqrt(){
     a('answer').value = Math.sqrt(x);
   }
 }
+
 function sqr(){
   if(a('answer').value == ''){
     a('answer').value = '';
@@ -85,6 +105,7 @@ function sqr(){
     a('answer').value = x*x;
   }
 }
+
 function factorial(){
   x = a('answer').value;
   let kq = 1;
@@ -93,8 +114,10 @@ function factorial(){
   a('answer').value = kq;
   }
 }
+
 function equal(){
   x = a('answer').value;
-  a('answer').value = eval(x)
+  let kq = x.replace(/x/g,'*').replace(/÷/g,'/').replace(/--/g,'+')
+  a('answer').value = eval(kq)
 }
 
