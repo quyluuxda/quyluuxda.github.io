bb0798cd227db37a08fb7988b9fad79592b0ee96
quyluuxda <48611997+quyluuxda@users.noreply.github.com>
2019-05-20 20:29:13 +0700

    validate form

--- bt_validate_form/js/script.js
+++ bt_validate_form/js/script.js
@@ -32,8 +32,8 @@ $('#content').on('submit', function () {
     } else {
         $('#address').next('span').text("");
     }
-    if ($('#dateofbirth').val().match(/^[0,1]?\d{1}\/(([0-2]?\d{1})|([3][0,1]{1}))\/(([1]{1}[9]{1}[9]{1}\d{1})|([2-9]{1}\d{3}))$/) == null) {
-        $('#dateofbirth').next('span').text("Dates of the form XX/XX/YYYY where XX can be 1 or 2 digits long and YYYY is always 4 digits long.");
+    if ($('#dateofbirth').val().match(/^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/) == null) {
+        $('#dateofbirth').next('span').text("Dates of birth is empty or invalid.");
         isValid = false;
     } else {
         $('#dateofbirth').next('span').text("");
