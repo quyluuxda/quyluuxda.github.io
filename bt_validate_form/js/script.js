0d1aff6e0b22e171f501c21f827ecad71f885ae9
quyluuxda <48611997+quyluuxda@users.noreply.github.com>
2019-05-21 07:25:20 +0700

    validate form

--- bt_validate_form/js/script.js
+++ bt_validate_form/js/script.js
@@ -33,7 +33,7 @@ $('#content').on('submit', function () {
         $('#address').next('span').text("");
     }
     if ($('#dateofbirth').val().match(/^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/) == null) {
-        $('#dateofbirth').next('span').text("Dates of birth is empty or invalid.");
+        $('#dateofbirth').next('span').text("dates of form is dd/mm/yyyy , dd/mm/yy, dd-mm-yyyy, dd-mm-yy.");
         isValid = false;
     } else {
         $('#dateofbirth').next('span').text("");
