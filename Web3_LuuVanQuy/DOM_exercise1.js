1d4db39ea980a47490c663b79a3352ac4e4a006a
quyluuxda <48611997+quyluuxda@users.noreply.github.com>
2019-06-23 10:51:01 +0700

    test js dom, logic

---
+++ Web3_LuuVanQuy/DOM_exercise1.js
@@ -0,0 +1,5 @@
+let x = document.getElementById("hplogo")
+x.setAttribute("src", "https://images-na.ssl-images-amazon.com/images/I/71-YTbQdjfL._SY355_.png")
+x.srcset=""
+let z = document.getElementsByClassName("gNO89b")
+z[1].value = 'yahoo'
