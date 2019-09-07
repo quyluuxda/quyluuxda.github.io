4b265e466062894ee97da467dd9d58c681e266f1
quyluuxda <quyluuxda@gmail.com>
2019-09-07 22:46:43 +0700

    test

--- react/js/script.js
+++ react/js/script.js
@@ -94,45 +94,28 @@
 // };
 
 
-
-// window.onload = function () {
-//     $.get("https://quyluuxda.github.io/react/data.json", function (data) {
-//         information = data
-//         for (let i = 0; i < data.information.length; i++) {
-//             $("#information").html(
-//                 `<tr>
-//             <td id="name">${data.information[0].name}</td>
-//             <td id="age">${data.information[0].born}</td>
-//             <td id="mail">${data.information[0].mail}</td>
-//             <td id="phone">${data.information[i].phone}</td>
-//             <td><i class="far fa-edit"> Chỉnh sửa</i> | <i class="far fa-trash-alt"> Xóa</i></td>
-//             </tr>`
-//             )
-
-//         }
-//     }
-// };
-
-
 window.onload = function () {
-  $.get("data.json", function (data) {
-    information = data
-    let x = data.information.length
+  $.get("http://localhost:3000/information", function (data) {
+    let x = data.length
     let string = ''
     for (i = 0; i < x; i++) {
       string += `<tr>
-              <td id="name">${data.information[i].name}</td>
-                <td id="age">${data.information[i].born}</td>
-                <td id="mail">${data.information[i].mail}</td>
-                <td id="phone">${data.information[i].phone}</td>
-                <td><i class="far fa-edit"> Chỉnh sửa</i> | <button><i onclick="del(${data.information[i].id})" class="far fa-trash-alt"> Xóa</i></button></td>
+              <td>${data[i].name}</td>
+                <td>${data[i].born}</td>
+                <td>${data[i].mail}</td>
+                <td>${data[i].phone}</td>
+                <td><a href="edit.html?id=${data[i].id}"><i class="far fa-edit"> Chỉnh sửa</i><a> |
+                  <button type="button" class="btn" data-toggle="modal" data-target="#exampleModal"> 
+                    <i onclick="del(${data[i].id}, this)" class="far fa-trash-alt"> Xóa</i> 
+                  </button>
+                </td>
                 </tr>`
       $("#information").html(string)
     }
   });
 };
 
-
+// AJax Post Method
 $("#post").click(function () {
   $.post("http://localhost:3000/information",
     {
@@ -147,19 +130,49 @@ $("#post").click(function () {
     });
 });
 
+// DELETE
+function del(id, hideTr) {
+  $('#yes').on('click', function (event) {
+    $.ajax({
+      url: `http://localhost:3000/information/${id}`,
+      method: 'DELETE',
+      contentType: 'application/json',
 
-function del(n){
-  $.ajax({
-    url: `http://localhost:3000/information/${n}`,
-    method: 'DELETE',
-    contentType: 'application/json',
-    success: function (result) {
-      // handle success
-    },
-    error: function (request, msg, error) {
-      // handle failure
-    }
-    
+      success: function (result) {
+        $(hideTr).parent().parent().parent().parent().hide(500)
+      },
+      // error: function (request, msg, error) {
+      //   // handle failure
+      // } 
+    })
   })
-  location.reload();
+  // location.reload();
 }
+// }
+
+let str = decodeURIComponent(window.location.href)
+x = str.split("id=")
+$.get(`http://localhost:3000/information/${x[1]}`, function (data) {
+  document.getElementById("inputNameEdit").value = data.name
+  document.getElementById("inputAgeEdit").value = data.born
+  document.getElementById("inputEmailEdit").value = data.mail
+  document.getElementById("inputPhoneEdit").value = data.phone
+});
+
+$("#edit").click(function () {
+  $.ajax({
+    url: `http://localhost:3000/information/${x[1]}`,
+    method: 'PUT',
+    data: `name=${document.getElementById("inputNameEdit").value}
+  &born=${document.getElementById("inputAgeEdit").value}
+  &mail=${document.getElementById("inputEmailEdit").value}
+  &phone=${document.getElementById("inputPhoneEdit").value}`,
+    succsess: function (data) {
+      alert('Load was performed.');
+    }
+  })
+  window.location.href = "index.html"
+})
+
+
+
